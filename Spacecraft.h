d879606ca75fcc2e6b0ff2bea68d8a0a3f883c01
Andreasgandhi <asg5725@psu.edu>
2024-12-11 00:39:29 -0500

    Update Spacecraft.h

--- Spacecraft.h
+++ Spacecraft.h
@@ -1,1 +1,29 @@
+//ALEX CHECK THIS 
+
+#ifndef SPACECRAFT_H
+#define SPACECRAFT_H
+
+#include <cmath>
+#include <atomic>
+#include <windows.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/glut.h>
+
+
+
+class Spacecraft 
+{
+private:
+    std::atomic<float> positionX;
+    std::atomic<float> positionY;
+    static constexpr float square_size = 50.0;
+
+public:
+    // Spacecraft constructor set to initial position
+    Spacecraft() : positionX(0.0), positionY(0.0) {}
+    void draw(float Xposition, float Yposition);
+};
+
+#endif // SPACECRAFT_H
 
